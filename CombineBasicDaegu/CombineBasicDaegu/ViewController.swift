3b374770093cf6fa9bf56db46d9f7522dbbf64c0
ollekil <ollekil@kakao.com>
2024-10-23 14:48:25 +0900

    First setup for CombineBasicDaegu

---
+++ CombineBasicDaegu/CombineBasicDaegu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CombineBasicDaegu
+//
+//  Created by 배정길 on 10/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CombineBasicDaegu/CombineBasicDaeguTests/CombineBasicDaeguTests.swift
@@ -0,0 +1,17 @@
+//
+//  CombineBasicDaeguTests.swift
+//  CombineBasicDaeguTests
+//
+//  Created by 배정길 on 10/23/24.
+//
+
+import Testing
+@testable import CombineBasicDaegu
+
+struct CombineBasicDaeguTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
